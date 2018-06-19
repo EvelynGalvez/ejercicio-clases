69c90a04f74924956f0a145ff110d60360cd74d9
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-19 11:47:31 -0400

    Agregado mensaje de espera y funcionalidad por cada botón

--- app.js
+++ app.js
@@ -1,72 +1,70 @@
 window.onload = () => {
-document.getElementById('espera').style.display = 'none';
 const btncats = document.getElementById('cats');
 const btndogs = document.getElementById('dogs');
 const btnbirds = document.getElementById('birds');
 
+
+
 btncats.addEventListener('click', () => {
-  /*document.getElementById('espera').style.display = 'block';*/
-  function obtenerGatos() {
-    // Fetch retorna una promesa
-    document.getElementById('espera').style.display = 'block';
-    fetch(`https://cors-anywhere.herokuapp.com/http://shibe.online/api/cats?count=3&urls=true&httpsUrls=true`)
-      .then((response) => { 
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Mala respuesta de gatitos");
-        }
-      }).then((catesJson) => { //recibimos el JSON en este punto
-        //Este then es de la promesa de response.json()
-        document.getElementById('espera').style.display = 'none';
-        const cateReceptorDiv = document.getElementById("cats");
-        for (let cateIndex = 0; cateIndex < catesJson.length; cateIndex++) {
-          const cateImg = document.createElement('img'); //Aquí "almaceno" las imágenes
-          cateImg.src = catesJson[cateIndex];
-          cateReceptorDiv.appendChild(cateImg);
-          /*document.getElementById('espera').style.display = 'none';*/
-        }
-      })
-      .catch((error) => {
-        console.error("Holi soy un error " + error);
-      });
-  
-  }
-  
-});
-}
-
-/*window.onload = () => {
-
+  document.getElementById('waiting').style.display = 'block';
+  document.getElementById('espera').style.display = 'block';
   obtenerGatos();
-  obtenerDoges();
-  obtenerBirds();
-};*/
-
-/*
+})
 
 btndogs.addEventListener('click', () => {
+  document.getElementById('waiting').style.display = 'block';
+  document.getElementById('espera').style.display = 'block';
   obtenerDoges();
-});
+})
 
 btnbirds.addEventListener('click', () => {
+  document.getElementById('waiting').style.display = 'block';
+  document.getElementById('espera').style.display = 'block';
   obtenerBirds();
-});
+}) 
+
+}
 
 
-
-function obtenerDoges() {
-  // Fetch retorna una promesa
-  fetch(`https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=3&urls=true&httpsUrls=true`)
+function obtenerGatos() {
+    // Fetch retorna una promesa
+  fetch(`https://cors-anywhere.herokuapp.com/http://shibe.online/api/cats?count=20&urls=true&httpsUrls=true`)
     .then((response) => { 
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("Mala respuesta de perritos");
+        throw new Error("No hay respuesta de gatitos");
+      }
+    }).then((catesJson) => { //recibimos el JSON en este punto
+        //Este then es de la promesa de response.json()
+      document.getElementById('waiting').style.display = 'none';
+      document.getElementById('espera').style.display = 'none';
+      const cateReceptorDiv = document.getElementById("contenedor-cats");
+      for (let cateIndex = 0; cateIndex < catesJson.length; cateIndex++) {
+        const cateImg = document.createElement('img'); //Aquí "almaceno" las imágenes
+        cateImg.src = catesJson[cateIndex];
+        cateReceptorDiv.appendChild(cateImg);
+      }
+      })
+      .catch(error => {
+        console.error("Error" + error);
+      });
+}
+
+function obtenerDoges() {
+  // Fetch retorna una promesa
+  fetch(`https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=10&urls=true&httpsUrls=true`)
+    .then((response) => { 
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("No hay respuesta de perritos");
       }
     }).then((dogesJson) => { //recibimos el JSON en este punto
       //Este then es de la promesa de response.json()
-      const dogeReceptorDiv = document.getElementById("dogs");
+      document.getElementById('waiting').style.display = 'none';
+      document.getElementById('espera').style.display = 'none';
+      const dogeReceptorDiv = document.getElementById("contenedor-dogs");
       for (let dogeIndex = 0; dogeIndex < dogesJson.length; dogeIndex++) {
         const dogeImg = document.createElement('img'); //Aquí "almaceno" las imágenes
         dogeImg.src = dogesJson[dogeIndex];
@@ -74,9 +72,10 @@ function obtenerDoges() {
       }
     })
     .catch((error) => {
-      console.error("Holi soy un error " + error);
+      console.error("Error" + error);
     });
 }
+
 
 function obtenerBirds() {
   // Fetch retorna una promesa
@@ -85,11 +84,11 @@ function obtenerBirds() {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("Mala respuesta de pajaritos");
+        throw new Error("No hay pajaritos");
       }
     }).then((birdsJson) => { //recibimos el JSON en este punto
       //Este then es de la promesa de response.json()
-      const birdReceptorDiv = document.getElementById("birds");
+      const birdReceptorDiv = document.getElementById("contenedor-birds");
       for (let birdIndex = 0; birdIndex < birdsJson.length; birdIndex++) {
         const birdImg = document.createElement('img'); //Aquí "almaceno" las imágenes
         birdImg.src = birdsJson[birdIndex];
@@ -97,7 +96,7 @@ function obtenerBirds() {
       }
     })
     .catch((error) => {
-      console.error("Holi soy un error " + error);
+      console.error("Error" + error);
     });
 }
-*/
+
